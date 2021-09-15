58625c50ab5ab327a252aab6044d601321bb5d8e
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-15 18:13:30 +0700

    Add mono_dirt and setup github actions

--- src/main/java/skullition/receptacle/Receptacle.java
+++ src/main/java/skullition/receptacle/Receptacle.java
@@ -1,13 +1,25 @@
 package skullition.receptacle;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Receptacle implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MODID = "receptacle";
+
+	public static final ItemGroup RECEPTACLE_ITEM_GROUP = FabricItemGroupBuilder
+			.build(new Identifier(Receptacle.MODID, "receptacle_item_group"), () -> new ItemStack(Setup.MONO_DIRT));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Setup.registerAll();
+		LOGGER.debug("onInitialize() called.");
 	}
 }
---
+++ src/main/java/skullition/receptacle/Setup.java
@@ -0,0 +1,23 @@
+package skullition.receptacle;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Setup {
+    public static final Block MONO_DIRT = new Block(FabricBlockSettings.of(Material.SOIL).strength(0.5F).sounds(BlockSoundGroup.GRAVEL));
+
+    public static void registerAll() {
+        // items
+        // blocks
+        Registry.register(Registry.BLOCK, new Identifier(Receptacle.MODID, "mono_dirt"), MONO_DIRT);
+        // block items
+        Registry.register(Registry.ITEM, new Identifier(Receptacle.MODID, "mono_dirt"), new BlockItem(MONO_DIRT, new FabricItemSettings().group(Receptacle.RECEPTACLE_ITEM_GROUP)));
+        // block entities
+    }
+}
