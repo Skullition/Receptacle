c0bbf4014f1bf551235a5133b5e7045d4c0068d5
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-16 20:23:44 +0700

    Add MonoGrassBlock

--- src/main/java/skullition/receptacle/Setup.java
+++ src/main/java/skullition/receptacle/Setup.java
@@ -8,16 +8,20 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import skullition.receptacle.blocks.MonoGrassBlock;
 
 public class Setup {
     public static final Block MONO_DIRT = new Block(FabricBlockSettings.of(Material.SOIL).strength(0.5F).sounds(BlockSoundGroup.GRAVEL));
+    public static final MonoGrassBlock MONO_GRASS_BLOCK = new MonoGrassBlock(FabricBlockSettings.of(Material.SOIL).strength(0.5F).sounds(BlockSoundGroup.GRASS));
 
     public static void registerAll() {
         // items
         // blocks
         Registry.register(Registry.BLOCK, new Identifier(Receptacle.MODID, "mono_dirt"), MONO_DIRT);
+        Registry.register(Registry.BLOCK, new Identifier(Receptacle.MODID, "mono_grass_block"), MONO_GRASS_BLOCK);
         // block items
         Registry.register(Registry.ITEM, new Identifier(Receptacle.MODID, "mono_dirt"), new BlockItem(MONO_DIRT, new FabricItemSettings().group(Receptacle.RECEPTACLE_ITEM_GROUP)));
+        Registry.register(Registry.ITEM, new Identifier(Receptacle.MODID, "mono_grass_block"), new BlockItem(MONO_GRASS_BLOCK, new FabricItemSettings().group(Receptacle.RECEPTACLE_ITEM_GROUP)));
         // block entities
     }
 }
---
+++ src/main/java/skullition/receptacle/blocks/abstractblocks/SpreadableMonoBlock.java
@@ -0,0 +1,62 @@
+package skullition.receptacle.blocks.abstractblocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SnowBlock;
+import net.minecraft.block.SnowyBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.chunk.light.ChunkLightProvider;
+import skullition.receptacle.Setup;
+
+import java.util.Random;
+
+public class SpreadableMonoBlock extends SnowyBlock {
+    protected SpreadableMonoBlock(Settings settings) {
+        super(settings);
+    }
+    private static boolean canSurvive(BlockState state, WorldView world, BlockPos pos) {
+        BlockPos blockPos = pos.up();
+        BlockState blockState = world.getBlockState(blockPos);
+        if (blockState.isOf(Blocks.SNOW) && (Integer)blockState.get(SnowBlock.LAYERS) == 1) {
+            return true;
+        } else if (blockState.getFluidState().getLevel() == 8) {
+            return false;
+        } else {
+            int i = ChunkLightProvider.getRealisticOpacity(world, state, pos, blockState, blockPos, Direction.UP, blockState.getOpacity(world, blockPos));
+            return i < world.getMaxLightLevel();
+        }
+    }
+
+    private static boolean canSpread(BlockState state, WorldView world, BlockPos pos) {
+        BlockPos blockPos = pos.up();
+        return canSurvive(state, world, pos) && !world.getFluidState(blockPos).isIn(FluidTags.WATER);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (!canSurvive(state, world, pos)) {
+            world.setBlockState(pos, Setup.MONO_DIRT.getDefaultState());
+        } else {
+            if (world.getLightLevel(pos.up()) >= 9) {
+                BlockState blockState = this.getDefaultState();
+
+                for(int i = 0; i < 4; ++i) {
+                    BlockPos blockPos = pos.add(random.nextInt(3) - 1, random.nextInt(5) - 3, random.nextInt(3) - 1);
+                    if (world.getBlockState(blockPos).isOf(Setup.MONO_DIRT) && canSpread(blockState, world, blockPos)) {
+                        world.setBlockState(blockPos, (BlockState)blockState.with(SNOWY, world.getBlockState(blockPos.up()).isOf(Blocks.SNOW)));
+                    }
+                }
+            }
+
+        }
+    }
+
+    @Override
+    public boolean hasRandomTicks(BlockState state) {
+        return true;
+    }
+}
