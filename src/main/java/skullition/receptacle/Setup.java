7a8d7fe828b1e31eca0cd4cd202db8b0f991a7e6
skullition <78258797+Skullition@users.noreply.github.com>
2021-09-18 10:36:36 +0700

    Add monobiome sound

--- src/main/java/skullition/receptacle/Setup.java
+++ src/main/java/skullition/receptacle/Setup.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import skullition.receptacle.blocks.MonoGrassBlock;
@@ -14,6 +15,7 @@ public class Setup {
     public static final Block MONO_DIRT = new Block(FabricBlockSettings.of(Material.SOIL).strength(0.5F).sounds(BlockSoundGroup.GRAVEL));
     public static final Block MONO_COBBLESTONE = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(2.0F, 6.0F));
     public static final MonoGrassBlock MONO_GRASS_BLOCK = new MonoGrassBlock(FabricBlockSettings.of(Material.SOIL).strength(0.5F).sounds(BlockSoundGroup.GRASS).ticksRandomly());
+    public static final SoundEvent MONO_AMBIENT = new SoundEvent(new Identifier(Receptacle.MODID, "monobiome"));
 
     public static void registerAll() {
         // items
@@ -26,5 +28,7 @@ public class Setup {
         Registry.register(Registry.ITEM, new Identifier(Receptacle.MODID, "mono_grass_block"), new BlockItem(MONO_GRASS_BLOCK, new FabricItemSettings().group(Receptacle.RECEPTACLE_ITEM_GROUP)));
         Registry.register(Registry.ITEM, new Identifier(Receptacle.MODID, "mono_cobblestone"), new BlockItem(MONO_COBBLESTONE, new FabricItemSettings().group(Receptacle.RECEPTACLE_ITEM_GROUP)));
         // block entities
+        // sounds
+        Registry.register(Registry.SOUND_EVENT, new Identifier(Receptacle.MODID, "monobiome"), MONO_AMBIENT);
     }
 }
