be61502aeb65deb21a60c6af75675878e3411e17
skullition <78258797+Skullition@users.noreply.github.com>
2021-10-02 12:20:15 +0700

    Add tooltip for PortableShelterItem

--- src/main/java/skullition/receptacle/items/PortableShelterItem.java
+++ src/main/java/skullition/receptacle/items/PortableShelterItem.java
@@ -2,13 +2,21 @@ package skullition.receptacle.items;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PortableShelterItem extends Item {
     public PortableShelterItem(Settings settings) {
@@ -36,6 +44,12 @@ public class PortableShelterItem extends Item {
         this.placeBlock(world, pos.south().add(0, 1, 0),glass);
 
         this.placeBlock(world, pos, Blocks.TORCH.getDefaultState());
+        ItemStack itemStack = user.getStackInHand(hand);
+        user.incrementStat(Stats.USED.getOrCreateStat(this));
+        if (!user.getAbilities().creativeMode) {
+            itemStack.decrement(1);
+        }
+
         return TypedActionResult.success(user.getStackInHand(hand));
     }
 
@@ -43,5 +57,10 @@ public class PortableShelterItem extends Item {
         if (world.getBlockState(pos).isAir()) {
             world.setBlockState(pos, state);
         }
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(1, new TranslatableText("portable_shelter_item.tooltip").formatted(Formatting.GRAY));
     }
 }
